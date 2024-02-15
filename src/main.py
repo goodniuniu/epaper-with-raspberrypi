76496c8e5001ef94c36ae66531d9671bc69a7d99
goodniuniu@gmail.com <goodniuniu@gmail.com>
2024-02-15 17:29:44 +0800

    finish get_weather.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # from weather_api import fetch_weather
 # from display import display_weather
 import get_config
+import get_weather
 import os
 
 def main():
@@ -8,6 +9,8 @@ def main():
     city_api_key = get_config.get_config_value('CITY_API_KEY')
     print(weather_api_key,city_api_key)
     # Add additional logic here to use weather_api_key and city_api_key
+    weather = get_weather.fetch_weather(weather_api_key,city_api_key)
+    print (weather)
 
 
 if __name__ == '__main__':
