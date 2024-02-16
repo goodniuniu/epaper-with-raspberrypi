f144dd464a5412b549fbc48dc9bd56dee06f22a4
goodniuniu@gmail.com <goodniuniu@gmail.com>
2024-02-16 15:19:31 +0800

    基于面向对象方式获取诗歌详情，将作为主要方法

--- src/main.py
+++ src/main.py
@@ -14,20 +14,22 @@ def main():
     poem_token_api_url = get_config.get_config_value('POEM_TOKEN_API_URL')
     daily_poem_api_url = get_config.get_config_value('DAILY_POEM_API_URL')
     # Add additional logic here to use weather_api_key and city_api_key
-    #weather = get_weather.fetch_weather(weather_api_key,city_api_key)
+    weather = get_weather.fetch_weather(weather_api_key,city_api_key)
+    print (weather)
+    
+    #基于面向过程的调用，如果需要测试可以采用。
     #poem_token_api = get_poem.get_token(poem_token_api_url)
     #poem = get_poem.get_poem_from_url(daily_poem_api_url,poem_token_api)
-    #print (weather)
     #print (poem)
     
 
-    # 获取诗歌详情
+    # 基于面向对象方式获取诗歌详情，将作为主要方法
     # 创建PoemAPI类的实例
-    api_url = "https://v2.jinrishici.com/sentence"
-    token_url = "https://v2.jinrishici.com/token"
+    ## 测试来源：
+    # api_url = "https://v2.jinrishici.com/sentence"
+    # token_url = "https://v2.jinrishici.com/token"
 
     poem_api = class_poem_api.PoemAPI(daily_poem_api_url, poem_token_api_url)
-
     # 获取诗歌详情
     if poem_api.get_poem_detail():
         logging.info("诗歌详情获取成功:")
