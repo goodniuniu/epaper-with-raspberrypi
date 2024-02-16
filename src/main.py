5086a6110077fe1270e528d7d7746008fe8cae0d
goodniuniu@gmail.com <goodniuniu@gmail.com>
2024-02-16 14:56:12 +0800

    step by step

--- src/main.py
+++ src/main.py
@@ -16,7 +16,6 @@ def main():
     # Add additional logic here to use weather_api_key and city_api_key
     #weather = get_weather.fetch_weather(weather_api_key,city_api_key)
     #poem_token_api = get_poem.get_token(poem_token_api_url)
-    logging.info(poem_token_api)    
     #poem = get_poem.get_poem_from_url(daily_poem_api_url,poem_token_api)
     #print (weather)
     #print (poem)
